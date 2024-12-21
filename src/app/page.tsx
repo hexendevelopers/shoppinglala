0a84b3d64ebdaf6992fa94a2f89b13d4d41361ec
hexendevelopers <hexenwebcreators@gmail.com>
2024-12-22 01:20:10 +0530

    Added All Calloections in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Header from "../Components/Header";
  import FeaturedProducts from "../Components/Category/FeaturedProducts";
+ import Hotdeals from "../Components/Category/Hotdeals";
+ import LatestProducts from "../Components/Category/LatestProduts";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -8,6 +10,9 @@ export default function Home() {
      <Header />
       
      <FeaturedProducts />
+      <Hotdeals />
+      <LatestProducts />
     </div>
   );
 }
+
